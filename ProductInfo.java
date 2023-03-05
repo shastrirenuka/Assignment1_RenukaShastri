a63f6568f137ebea3f42a982ce82d8521860826c
Renuka <shastrirenukap8@gmail.com>
2023-03-05 16:20:38 +0000

    second commit ProductInfo.java

---
+++ ProductInfo.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+import java.awt.*;
+
+class ProductDetails
+
+{
+
+public static voiid main(String args[])
+
+{
+
+System.out.println("\n Share Details");
+
+System.out.println("\n Share Name:");
+
+System.out.println("\n Share Price:");
+
+System.out.println("\n Issue Date:);
+
+System.out.println("\n Face value:);
+
+}
+
+}
